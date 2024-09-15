38a11810b1bd2baa9e17207c7ee59f20bc6fb91c
takashi <tks.srtr89@gmail.com>
2024-09-15 14:00:07 +0900

    add: many0

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
 use nom::{
     bytes::complete::tag,
     error::{Error, ErrorKind},
+    multi::many0,
     sequence::delimited,
     Err, IResult,
 };
 
 fn main() {
-    // tag
+    /*
+     *  tag
+     */
     fn parser(s: &str) -> IResult<&str, &str> {
         tag("Hello")(s)
     }
@@ -17,10 +20,28 @@ fn main() {
     );
     assert_eq!(parser(""), Err(Err::Error(Error::new("", ErrorKind::Tag))));
 
-    // delimited
-    let mut parser = delimited(tag("("), tag("abc"), tag(")"));
-    assert_eq!(parser("(abc)"), Ok(("", "abc")));
-    assert_eq!(parser("(abc)def"), Ok(("def", "abc")));
-    assert_eq!(parser(""), Err(Err::Error(("", ErrorKind::Tag))));
-    assert_eq!(parser("123"), Err(Err::Error(("123", ErrorKind::Tag))));
+    /*
+     * delimited
+     */
+    let mut delimited_parser = delimited(tag("("), tag("abc"), tag(")"));
+    assert_eq!(delimited_parser("(abc)"), Ok(("", "abc")));
+    assert_eq!(delimited_parser("(abc)def"), Ok(("def", "abc")));
+    assert_eq!(delimited_parser(""), Err(Err::Error(("", ErrorKind::Tag))));
+    assert_eq!(
+        delimited_parser("123"),
+        Err(Err::Error(("123", ErrorKind::Tag)))
+    );
+
+    /*
+     * many0
+     */
+
+    fn many0_parser(s: &str) -> IResult<&str, Vec<&str>> {
+        many0(tag("abc"))(s)
+    }
+
+    assert_eq!(many0_parser("abcabc"), Ok(("", vec!["abc", "abc"])));
+    assert_eq!(many0_parser("abc123"), Ok(("123", vec!["abc"])));
+    assert_eq!(many0_parser("123123"), Ok(("123123", vec![])));
+    assert_eq!(many0_parser(""), Ok(("", vec![])));
 }
