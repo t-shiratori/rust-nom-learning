3c409290a6685954e89b66a16025889ee03e69ea
takashi <tks.srtr89@gmail.com>
2024-09-15 12:28:05 +0900

    update

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,15 @@
 use nom::{
     bytes::complete::tag,
     error::{Error, ErrorKind},
+    sequence::delimited,
     Err, IResult,
 };
 
 fn main() {
+    // tag
     fn parser(s: &str) -> IResult<&str, &str> {
         tag("Hello")(s)
     }
-
     assert_eq!(parser("Hello, World!"), Ok((", World!", "Hello")));
     assert_eq!(
         parser("Something"),
@@ -16,7 +17,10 @@ fn main() {
     );
     assert_eq!(parser(""), Err(Err::Error(Error::new("", ErrorKind::Tag))));
 
-    //     let mut parser = delimited(tag("("), tag("abc"), tag(")"));
-    // assert_eq!(parser("(abc)"), Ok(("", "abc")));
-    // assert_eq!(parser("(abc)def"), Ok(("def", "abc")));
+    // delimited
+    let mut parser = delimited(tag("("), tag("abc"), tag(")"));
+    assert_eq!(parser("(abc)"), Ok(("", "abc")));
+    assert_eq!(parser("(abc)def"), Ok(("def", "abc")));
+    assert_eq!(parser(""), Err(Err::Error(("", ErrorKind::Tag))));
+    assert_eq!(parser("123"), Err(Err::Error(("123", ErrorKind::Tag))));
 }
